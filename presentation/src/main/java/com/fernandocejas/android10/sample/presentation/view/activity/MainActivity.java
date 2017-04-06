121f042880a69779a9c0256d50a0bc5fdb0c4d46
Gayazov Samat <diesersamat@mac430rih.rambler.ramblermedia.com>
2017-04-06 11:58:07 +0300

    fd

--- presentation/src/main/java/com/fernandocejas/android10/sample/presentation/view/activity/MainActivity.java
+++ presentation/src/main/java/com/fernandocejas/android10/sample/presentation/view/activity/MainActivity.java
@@ -2,30 +2,38 @@ package com.fernandocejas.android10.sample.presentation.view.activity;
 
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.TextView;
+
+import com.fernandocejas.android10.sample.presentation.BuildConfig;
+import com.fernandocejas.android10.sample.presentation.R;
+
 import butterknife.Bind;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
-import com.fernandocejas.android10.sample.presentation.R;
 
 /**
  * Main application screen. This is the app entry point.
  */
 public class MainActivity extends BaseActivity {
 
-  @Bind(R.id.btn_LoadData) Button btn_LoadData;
+    @Bind(R.id.btn_LoadData)
+    Button btn_LoadData;
+    @Bind(R.id.id)
+    TextView id;
 
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
-    ButterKnife.bind(this);
-  }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        id.setText(BuildConfig.APPLICATION_ID);
+    }
 
-  /**
-   * Goes to the user list screen.
-   */
-  @OnClick(R.id.btn_LoadData)
-  void navigateToUserList() {
-    this.navigator.navigateToUserList(this);
-  }
+    /**
+     * Goes to the user list screen.
+     */
+    @OnClick(R.id.btn_LoadData)
+    void navigateToUserList() {
+        this.navigator.navigateToUserList(this);
+    }
 }
