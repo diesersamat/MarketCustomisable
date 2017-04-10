5f53291e912ed230c84c02782a9cfe1bf333786c
Gayazov Samat <diesersamat@mac430rih.rambler.ramblermedia.com>
2017-04-10 10:02:44 +0300

    ds

--- presentation/src/main/java/com/fernandocejas/android10/sample/presentation/view/activity/UserListActivity.java
+++ presentation/src/main/java/com/fernandocejas/android10/sample/presentation/view/activity/UserListActivity.java
@@ -9,6 +9,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Window;
+
 import com.fernandocejas.android10.sample.presentation.R;
 import com.fernandocejas.android10.sample.presentation.internal.di.HasComponent;
 import com.fernandocejas.android10.sample.presentation.internal.di.components.DaggerUserComponent;
@@ -20,37 +21,40 @@ import com.fernandocejas.android10.sample.presentation.view.fragment.UserListFra
  * Activity that shows a list of Users.
  */
 public class UserListActivity extends BaseActivity implements HasComponent<UserComponent>,
-    UserListFragment.UserListListener {
+        UserListFragment.UserListListener {
 
-  public static Intent getCallingIntent(Context context) {
-    return new Intent(context, UserListActivity.class);
-  }
+    private UserComponent userComponent;
 
-  private UserComponent userComponent;
-
-  @Override protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
-    setContentView(R.layout.activity_layout);
-
-    this.initializeInjector();
-    if (savedInstanceState == null) {
-      addFragment(R.id.fragmentContainer, new UserListFragment());
+    public static Intent getCallingIntent(Context context) {
+        return new Intent(context, UserListActivity.class);
     }
-  }
 
-  private void initializeInjector() {
-    this.userComponent = DaggerUserComponent.builder()
-        .applicationComponent(getApplicationComponent())
-        .activityModule(getActivityModule())
-        .build();
-  }
+    @Override
+    public UserComponent getComponent() {
+        return userComponent;
+    }
 
-  @Override public UserComponent getComponent() {
-    return userComponent;
-  }
+    @Override
+    public void onUserClicked(UserModel userModel) {
+        this.navigator.navigateToUserDetails(this, userModel.getUserId());
+    }
 
-  @Override public void onUserClicked(UserModel userModel) {
-    this.navigator.navigateToUserDetails(this, userModel.getUserId());
-  }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
+        setContentView(R.layout.activity_layout);
+
+        this.initializeInjector();
+        if (savedInstanceState == null) {
+            addFragment(R.id.fragmentContainer, new UserListFragment());
+        }
+    }
+
+    private void initializeInjector() {
+        this.userComponent = DaggerUserComponent.builder()
+                .applicationComponent(getApplicationComponent())
+                .activityModule(getActivityModule())
+                .build();
+    }
 }
